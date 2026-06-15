821eef864fd8eb52b184125aa384488dc36f141b
Marco <marcojslabbert@protonmail.com>
2026-06-15 06:18:20 +0200

    removed rig-forge from my branch

--- rig-forge/src/db/index.ts
+++
@@ -1,6 +0,0 @@
-import { drizzle } from 'drizzle-orm/node-postgres'
-
-import * as schema from './schema.ts'
-import { env } from '@/env.ts'
-
-export const db = drizzle(env.DATABASE_URL, { schema })
